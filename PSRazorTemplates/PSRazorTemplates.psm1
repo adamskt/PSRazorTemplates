acc9b960facd30ceff9a6451bfee3fe72536beae
adamskt <kelly@intellitect.com>
2017-09-05 16:45:15 -0700

    Get path to RazorLight assembly

--- PSRazorTemplates/PSRazorTemplates.psm1
+++ PSRazorTemplates/PSRazorTemplates.psm1
@@ -1,23 +1,26 @@
+# Test if .NET Framework 4.5.1 is installed
+if (Get-ChildItem "hklm:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\" |
+        Get-ItemPropertyValue -Name Release |
+        ForEach-Object { $_ -ge 378675 }) {
+            $RazorLightAssemblyPath = Join-path $PSScriptRoot "net451\RazorLight.dll"
+}
+
+#TODO: Test if .Net Core 1.0 (NETSTANDARD 1.6) is installed
 
 
-if( -not ($Library = Add-Type -path $RazorLightAssemblyPath -PassThru -ErrorAction stop) )
-{
+if ( -not ($Library = Add-Type -path $RazorLightAssemblyPath -PassThru -ErrorAction stop) ) {
     Throw "This module requires the RazorLight assembly, which needs either the .Net 4.5.1 framework or .Net Standard 1.6"
 }
 
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach($import in @($Public + $Private))
-{
-    Try
-    {
+Foreach ($import in @($Public + $Private)) {
+    Try {
         . $import.fullname
-    }
-    Catch
-    {
+    } Catch {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
