8d4aeaafd6a489497c05039b83048c20ed7d1fd7
adamskt <kelly@intellitect.com>
2017-09-06 16:43:27 -0700

    Added CS proj

---
+++ PSRazorTemplates/InvokeTemplateCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Management.Automation;
+
+namespace PSRazorTemplates
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "Template")]
+    public class InvokeTemplateCommand : Cmdlet
+    {
+        
+    }
+}
